d0c6fe8a994796a8ba8c627e1e5d78e50f777b6c
Versh-03 <25113cn167@glbitm.ac.in>
2026-08-23 08:39:25 +0530

    problem 75

---
+++ 75-SortColors.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left=0,mid=0;
+        int right=nums.size()-1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[left],nums[mid]);
+                left++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[right]);
+                right--;
+            }
+        }
+    }
+};
