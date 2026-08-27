f9d27a0efb427fc68990f8d94e94ac144621c954
Versh-03 <25113cn167@glbitm.ac.in>
2026-08-27 23:11:32 +0530

    Create 287-FindTheDuplicateNo.cpp

---
+++ 287-FindTheDuplicateNo.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int x: nums){
+            mp[x]++;
+        }
+        for(auto x: mp){
+            if(x.second>1){
+                return x.first;
+            }
+        }
+        return 0;
+    }
+};
