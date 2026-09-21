237c54dfb2f23881920a4d7c0662f6860cac0a8d
Versh-03 <25113cn167@glbitm.ac.in>
2026-09-21 10:48:58 +0530

    Create 15-3Sum.cpp

---
+++ 15-3Sum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        int sum=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left=i+1;
+            int right=nums.size()-1;
+
+            while(left<right){
+                if(-nums[i]==nums[left]+nums[right]){
+                    result.push_back({nums[i],nums[left],nums[right]});
+                    left++;
+                    right--;
+
+                    while(left < right && nums[left] == nums[left-1])
+                        left++;
+                    while(left < right && nums[right] == nums[right+1])
+                        right--;
+                }
+
+                else if(-nums[i]>nums[left]+nums[right]) left++;
+                else  right--;
+            }
+
+        }
+        return result;
+
+    }
+};
